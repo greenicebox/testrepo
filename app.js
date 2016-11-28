b73505ff227d1bb7e6ddb3b54555762d0ffbfa7a
sverma <sverma@movoto.com>
2016-11-28 16:25:21 +0530

    some more changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 (function(){
  console.log('hello world');
  console.log('hotfix 1');
+ console.log('some changes here');
  console.log('issue 21');
  console.log('some other line');
  console.log('merged into edit');
